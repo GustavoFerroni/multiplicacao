d74d1cb487e732db76e4016e36ab802e4af4c3fd
GustavoFerroni <gustavoffelipe13012010@gmail.com>
2025-04-30 13:27:16 -0300

    Add files via upload

---
+++ multiplicacao.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.multiplicacao;
+
+/**
+ *
+ * @author dti
+ */
+public class multiplicacao {
+    public static void main(String[] args) {
+        double numero1 = 7;
+        double numero2 = 5;
+        double resultado = numero1 * numero2;
+        System.out.println("A multiplicação é: " + resultado);
+    }
+}
+
+    
+
